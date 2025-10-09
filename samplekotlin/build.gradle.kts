00447f25eb861c91c722c488f24da3c666d73525
Irving Vega <irvingb@bluetrailsoft.com>
2025-10-09 11:11:47 -0600

    chore: bump Compose BOM to ensure 16KB alignment compliance

--- samplekotlin/build.gradle.kts
+++ samplekotlin/build.gradle.kts
@@ -93,10 +93,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
 
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.accompanist:accompanist-webview:0.35.0-alpha")
